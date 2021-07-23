6b6d49e47ea6c9647cbf101baaea8c0e66d489f5
Victor <lucckyboy713@gmail.com>
2021-07-23 16:55:02 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+//API 91d4c7a847e36aaa33ef6d7c116eef96
+
+const iconElement = document.querySelector('.weather.icon')  
+const tempElement = document.querySelector('.temperature.value p')
+const descElement = document.querySelector('.temperature-description')
+const locationElement = document.querySelector('.location p')
+const notificationElement = document.querySelector('.notification')
+
+//App Data
+const weather ={};
+weather.temperature = {
+    unit: 'celsius'
+};
+
+// Const and Variables
+const Kelvin = 273;
+const key = '91d4c7a847e36aaa33ef6d7c116eef96';
+
+//Check if the browser support GEO
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = `<p> Браузер не поддерживает Геолокацию`
+}
+
+// Set user position
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude,longitude);
+}
+
+// Show error when we have issue with GEO service
+function showError(error){
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = `<p> ${error.message}`
+}
+
+//Get weather from API
+function getWeather(latitude,longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
+fetch(api)
+    .then(function(response){
+        let data = response.json();
+        return data;
+    })
+    .then(function(data){
+        weather.temperature.value = Math.floor(data.main.temp = Kelvin)
+        weather.description = data.weather[0].description;
+        weather.iconId = data.weather[0].icon;
+        weather.city = data.name;
+        weather.country = data.sys.country;
+    })
+    .then(function(){
+        displayWeather();
+    });
+}
+
+//Display Weather to UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png`
+    tempElement.innerHTML = `${weather.temperature.value} ° <span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
